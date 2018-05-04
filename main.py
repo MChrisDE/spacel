776ebb7c4376c7a4496f9053203f32096a3a1cb3
MChrisDE <mchris@outlook.de>
2018-05-04 16:54:32 +0200

    added powerup

--- main.py
+++ main.py
@@ -31,10 +31,18 @@ class Game(FloatLayout):
             json_data = json.load(json_file)
         self.json_data = json_data
         self.clock = Clock.schedule_interval(self.update, 1.0 / 60.0)
+        self.spawn_clock = Clock.schedule_interval(self.spawn, 1.0 / 60.0)
         self.walls = []
+        self.powerups = []
         self.speed = 1
         self.time = 120
         self.score = 0
+
+    def spawn(self, dt):
+        if randint(0, int(self.time)) == 1:
+            self.spawn_wall()
+        if randint(0, 400) == 1:
+            self.spawn_powerup()
 
     def update(self, dt):
         self.player.move()
@@ -42,12 +50,20 @@ class Game(FloatLayout):
         for i in self.walls:
             if self.player.collide_widget(i):
                 self.stop()
+        for i in self.powerups:
+            if self.player.collide_widget(i):
+                self.powerups.remove(i)
+                i.__del__()
+                self.player.velocity += 1
         if self.time > 20:
             self.time -= 0.1
         if self.speed < 3:
             self.speed += 0.0005
-        if randint(0, int(self.time)) == 1:
-            self.spawn_wall()
+
+    def spawn_powerup(self):
+        d = SpeedUp()
+        self.powerups.append(d)
+        self.add_widget(d)
 
     def spawn_wall(self):
         d = Wall(self.speed)
@@ -63,6 +79,7 @@ class Game(FloatLayout):
 
     def stop(self):
         self.clock.cancel()
+        self.spawn_clock.cancel()
         if self.score > self.json_data["highscore"]:
             self.json_data["highscore"] = self.score
             with open('data.json', 'w') as outfile:
@@ -75,11 +92,16 @@ class Game(FloatLayout):
         if self.walls:
             self.clear_widgets(children=self.walls)
             self.walls = []
+        if self.powerups:
+            self.clear_widgets(children=self.powerups)
+            self.powerups = []
         self.clear_widgets(children=[self.menu])
         self.time = 120
         self.score = 0
         self.player.reset()
         self.clock()
+        self.speed = 1
+        self.spawn_clock()
 
 
 class Player(Widget):
@@ -90,6 +112,7 @@ class Player(Widget):
         self.controller = (Window.width * .09, Window.height * .16)
         self.touch = True
         self.pos = (Window.width * .5, Window.height * .5)
+        self.velocity = 5
 
     def on_touch_move(self, touch):
         self.on_touch_down(touch)
@@ -97,7 +120,7 @@ class Player(Widget):
 
     def on_touch_down(self, touch):
         self.vector = Vector(touch.x - self.controller[0],
-                             touch.y - self.controller[1]).normalize() * 5 * Window.width / 1920
+                             touch.y - self.controller[1]).normalize() * self.velocity * Window.width / 1920
         self.touch = False
 
     def on_touch_up(self, touch):
@@ -121,6 +144,17 @@ class Controls(Widget):
     pass
 
 
+class SpeedUp(Widget):
+    def __init__(self, **kwargs):
+        super(SpeedUp, self).__init__(**kwargs)
+        self.size_hint = (50 / Window.width, 50 / Window.height)
+        self.x = randint(0, Window.width - (self.size_hint[0] * Window.width))
+        self.y = randint(0, Window.height - (self.size_hint[1] * Window.height))
+
+    def __del__(self):
+        self.canvas.clear()
+
+
 class Wall(Widget):
     def __init__(self, speed, **kwargs):
         super(Wall, self).__init__(**kwargs)
@@ -129,14 +163,14 @@ class Wall(Widget):
 
         if self.direction == 0 or self.direction == 2:
             self.size_hint = (randint(50, 100) / Window.width, randint(50, 300) / Window.height)
-            self.y = randint(0, Window.height - self.height)
+            self.y = randint(0, Window.height - (self.size_hint[1] * Window.height))
             if self.direction == 0:
                 self.x = -self.size_hint[0] * Window.width
             else:
                 self.x = Window.width
         if self.direction == 1 or self.direction == 3:
             self.size_hint = (randint(50, 300) / Window.width, randint(50, 100) / Window.height)
-            self.x = randint(0, Window.width - self.width)
+            self.x = randint(0, Window.width - (self.size_hint[0] * Window.width))
             if self.direction == 1:
                 self.y = -self.size_hint[1] * Window.height
             else:
@@ -157,6 +191,7 @@ class SpaceL(App):
     def build(self):
         self.load_kv('main.kv')
         self.game = Game()
+        self.game.stop()
         return self.game
 
 
